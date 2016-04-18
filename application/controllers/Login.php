e74cbecc0d0424500d757d1f41aae7825496f07e
pedro <pedro@tinvest.com.br>
2016-04-17 21:07:33 -0300

    mudanças nas configurações

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,11 +1,13 @@
 <?php 
 if ( !defined('BASEPATH')) exit('No direct script access allowed');
 class Login extends CI_Controller {
+	
 	public function __construct(){
 		parent::__construct();
 		$this->load->library ( 'form_validation' );
 		$this->load->model("usuarios_model",'model');
 	}
+	
 	public function index(){
 		$conf = array(
 			'tela'=>'login/body'
